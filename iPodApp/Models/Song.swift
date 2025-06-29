9908259ebc7c3ffe1eb219aa6e3402eb8954f90e
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-06-29 14:40:02 +0400

    music model

---
+++ iPodApp/Models/Song.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+struct Song: Identifiable, Equatable {
+    let id = UUID()
+    let title: String
+    let artist: String
+    let album: String
+    let duration: TimeInterval
+    let fileURL: URL?
+    let artworkData: Data?
+    
+    init(title: String, artist: String, album: String, duration: TimeInterval, fileURL: URL? = nil, artworkData: Data? = nil) {
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self.duration = duration
+        self.fileURL = fileURL
+        self.artworkData = artworkData
+    }
+    
+    var formattedDuration: String {
+        let minutes = Int(duration) / 60
+        let seconds = Int(duration) % 60
+        return String(format: "%d:%02d", minutes, seconds)
+    }
+    
+    static var mockSongs: [Song] {
+        [
+            Song(title: "Hey Jude", artist: "The Beatles", album: "The Beatles Again", duration: 431),
+            Song(title: "Bohemian Rhapsody", artist: "Queen", album: "A Night at the Opera", duration: 354),
+            Song(title: "Hotel California", artist: "Eagles", album: "Hotel California", duration: 391),
+            Song(title: "Stairway to Heaven", artist: "Led Zeppelin", album: "Led Zeppelin IV", duration: 482),
+            Song(title: "Imagine", artist: "John Lennon", album: "Imagine", duration: 183)
+        ]
+    }
+}
